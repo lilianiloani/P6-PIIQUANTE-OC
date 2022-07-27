0c872348583b0e1960fbb2e39382acf8dde81549
Lilian <chinweiloani@yahoo.com>
2022-07-27 18:39:29 +0200

    First changes

--- app.js
+++ app.js
@@ -2,6 +2,29 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Requête reçue !');
+  next();
+});
 
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Votre requête a bien été reçue !' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Réponse envoyée avec succès !');
+});
 
 module.exports = app;
+// dans un premier terminal
+// cd /back
+// nodemon server ==>  Run back on http://localhost:3000
+// dans un second terminal
+// cb /front
+// npm run start => Run front on http://localhost:4200
