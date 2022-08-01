973568ef80d20972daf92162bdf6dfacc936d903
Lilian <chinweiloani@yahoo.com>
2022-08-01 15:28:10 +0200

    connect mongoDB

--- app.js
+++ app.js
@@ -1,30 +1,79 @@
+// - Connect the app to mongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://chinwe360:Kirsten2012@cluster0.aqhcnzm.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+const Thing = require('./models/thing');
+
+const { application } = require('express');
 const express = require('express');
 
 const app = express();
-
+app.use(express.json())
+  
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
+app.use('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error })); 
 });
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
+app.use('/api/stuff', (req, res, next) => {
+  Thing.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
 });
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
+app.post('/api/auth/login', (req, res, next) => {
+  console.log('Login request received !');
+  console.log('Body data', req.body)
+  
+  res.status(200).json({userId: 1});
+})
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => {
+  Thing.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+    .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
+
+//TODO : 
+  // - Check if user with same email and password exist
+  // IF true return userId  / tken
+  // If false return error Invalid Credential
+  // Todo: create route which return sauces : /api/sauces
+
+
 // dans un premier terminal
 // cd /back
 // nodemon server ==>  Run back on http://localhost:3000
 // dans un second terminal
 // cb /front
-// npm run start => Run front on http://localhost:4200
+// npm run start => Run front on http://localhost:4200npm run 
